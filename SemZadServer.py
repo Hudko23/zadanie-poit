9f81a306c4aae3670a916721c21b48f2ee0822e5
Ado <adrian.hudek10@gmail.com>
2020-05-29 21:33:05 +0200

    Delete comment

--- SemZadServer.py
+++ SemZadServer.py
@@ -1,11 +1,3 @@
-#import serial
-
-#ser=serial.Serial("/dev/ttyUSB0",9600)
-#ser.baudrate=9600
-
-#while True:
-#    read_ser=ser.readline()
-#    print(read_ser)
 
 from threading import Lock
 from flask import Flask, render_template, session, request, jsonify, url_for
