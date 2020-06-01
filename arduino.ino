ca0c5b293e8e14f1e6e4c2d2b75e6d477d4b5c29
Hudko23 <64903273+Hudko23@users.noreply.github.com>
2020-06-01 21:05:24 +0200

    Add arduino code

---
+++ arduino.ino
@@ -0,0 +1,65 @@
+int CarRed = 12;
+int CarYellow = 11;
+int CarGreen = 10;
+int WalkerRed = 9;
+int WalkerGreen = 8;
+int ButtonForWalker = 2;
+int FotosensorForCar = A0;
+String CarLightRed = "CarRed";
+String CarLightGreen = "CarGreen";
+String WalkerLightRed = "WalkerRed";
+String WalkerLightGreen = "WalkerGreen";
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(CarRed, OUTPUT);
+    pinMode(CarYellow, OUTPUT);
+    pinMode(CarGreen, OUTPUT);
+    pinMode(WalkerRed, OUTPUT);
+    pinMode(WalkerGreen, OUTPUT);
+    pinMode(ButtonForWalker, INPUT);
+    pinMode(FotosensorForCar, INPUT);
+    digitalWrite(CarGreen, HIGH);
+    Serial.println(CarLightGreen);
+    digitalWrite(WalkerRed, HIGH);
+    Serial.println(WalkerLightRed);
+}
+
+void loop() {
+   int StatusButton = digitalRead(ButtonForWalker);
+   int StatusFotosensor = analogRead(FotosensorForCar);
+   
+      if (StatusButton == HIGH) {
+
+        digitalWrite(CarGreen, LOW);
+        digitalWrite(CarYellow, HIGH);
+        delay(2000);
+      
+        digitalWrite(CarYellow, LOW);
+        digitalWrite(CarRed, HIGH);
+        Serial.println(CarLightRed);
+        delay(1000);
+      
+        digitalWrite(WalkerRed, LOW);
+        digitalWrite(WalkerGreen, HIGH);
+        Serial.println(WalkerLightGreen);
+       
+      }
+
+      else if (StatusFotosensor < 100) {
+        
+        digitalWrite(WalkerGreen, LOW);
+        digitalWrite(WalkerRed, HIGH);
+        Serial.println(WalkerLightRed);
+        delay(1000);
+      
+        digitalWrite(CarYellow, HIGH);
+        delay(2000);
+        
+        digitalWrite(CarRed, LOW);
+        digitalWrite(CarGreen, HIGH);
+        Serial.println(CarLightGreen);
+        digitalWrite(CarYellow, LOW);
+      
+      }      
+}
